cc19f14cd2af34061e38c7f06008fc0c5256c1de
IDoston <dostoninogomov3@gmail.com>
2022-02-20 16:02:03 +0300

    my tour

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var result=prompt("qancha pulingiz bor")
+
+var usdToUzs=(750*10900);
+var euroToUzs=(120*12272);
+var isTotal = (usdToUzs + euroToUzs)
+
+if(result >= isTotal){
+    console.log("Oq yo'l, Alisher!")
+}else{
+    console.log("Alisher, ozgina sabr qilish kerak bo’lar ekan.")
+}
